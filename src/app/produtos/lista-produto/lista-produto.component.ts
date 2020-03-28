9db91ce58764359a03f0c214d12c8a73017ddf26
Joyce Sena <joycesenanet@gmail.com>
2020-03-28 11:40:59 -0300

    projeto em angular

---
+++ src/app/produtos/lista-produto/lista-produto.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Produto } from '../produto';
+import { ProdutoService } from '../produtos.service';
+
+@Component({
+  selector: 'app-lista-produto',
+  templateUrl: './lista-produto.component.html'
+})
+export class ListaProdutoComponent implements OnInit {
+
+  constructor(private produtoService: ProdutoService) { }
+
+  public produtos: Produto[];
+
+  ngOnInit() {
+    this.produtoService.obterProdutos()
+      .subscribe(
+        produtos => {
+          this.produtos = produtos;
+          alert('Retorno da API - Sucesso! ');
+         // console.log(produtos);
+        },
+        error => console.log(error)
+      );
+  }
+
+
+}
+
